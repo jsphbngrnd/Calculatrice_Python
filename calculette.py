d4c42fa9355051ce1029218aa060729afa4694ff
Joseph Bongrand <josephbongrand@Josephs-MacBook-Pro.local>
2019-12-17 14:20:17 +0100

    Annonce des variables

--- calculette.py
+++ calculette.py
@@ -1,1 +1,8 @@
 print('Début travail calculette')
+
+# On déclare les différentes variables
+
+select = input("Sélectionner le type d'opération 1,2,3,4:")
+
+Donnée_1 = int(input("Entrez premier chiffre"))
+Donnée_2 = int(input("Entrez second chiffre"))
